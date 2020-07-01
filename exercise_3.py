86862a7c70d558eb7644336c7cb764894d9753b4
AndreyeuIvan <andreeviv@icloud.com>
2020-07-02 01:37:56 +0300

    added exercises 3,4,42,43

---
+++ exercise_3.py
@@ -0,0 +1,19 @@
+'''Numbers and math'''
+
+print('I will now count my chickens:')# writing a string
+
+print('Hens', 25 + 30 / 6) # counting chickens
+print('Ropsters', 100 - 25 * 3 %4)
+
+print('Now I will count the eggs:')
+
+print(3.2+2.3+1.3-5.4+45.5%2.3-1.5/45.4+6.5)
+print('It is true that 3+2<5-7?')
+print(3+2<5-7)
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+print("How about some more.")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print('Is it less or equal', 5<= -2)
