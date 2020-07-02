9b9a6e0b3831bb869d9d037c32f6b3aa9e9abdc0
ivar <andreeviv@cloud.com>
2020-07-02 16:55:47 +0300

    my own classeshave been created

---
+++ exercise_45.py
@@ -0,0 +1,22 @@
+'''Creatin own game with a plot, of how to become a python_programmer'''
+class Programmer:
+    pass
+
+class BackEnd(Programmer):
+    pass
+
+
+class Frontend(Programmer):
+    pass
+
+
+class English:
+    pass
+
+
+class Friends:
+    pass
+
+
+class Company:
+    pass
