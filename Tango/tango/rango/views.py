16c9cb3c73f069cc3c2b3d47615a857a12158782
ivar <andreeviv@icloud.com>
2020-07-07 18:11:48 +0300

    created Tango templates directory

--- Tango/tango/rango/views.py
+++ Tango/tango/rango/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Rango starts <a href="/ "> About </a>')
+    context = "I am bold font from the context"
+    return render(request, 'rango/index.html', {'boldmessage': context})
+
 
 def hello(request):
     return HttpResponse('Rango Says: <a href="/about/"> Index </a> page.')
